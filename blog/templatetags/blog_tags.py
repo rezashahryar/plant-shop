57107de3a82b051643ffa1f4ad64eb5c9f811ab5
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-09-27 20:00:23 +0330

    implement recent post & recent comments in sidebar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from blog.models import Post
+from blog.models import Comment, Post
 
 # create your templatetags here
 
@@ -11,3 +11,9 @@ register = template.Library()
 def recent_post():
     posts = Post.objects.filter(published_status=True)[:4]
     return {'posts': posts}
+
+
+@register.inclusion_tag('includes/recent_comments.html')
+def recent_comments_of_blog_section():
+    comments = Comment.objects.select_related('user').select_related('post').all()[:4]
+    return {'comments': comments}
